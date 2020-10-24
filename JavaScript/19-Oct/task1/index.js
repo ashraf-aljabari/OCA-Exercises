eee3d6867fdf753bfce456a546c2d03a104af0c4
ashraf-aljabari <ashraf.n.aljabari@gmail.com>
2020-10-25 01:27:36 +0300

    task is done

--- JavaScript/19-Oct/task1/index.js
+++ JavaScript/19-Oct/task1/index.js
@@ -2,12 +2,14 @@ var emailReg = new RegExp(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/);
 var passwordReg = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
 var phoneNumberReg = new RegExp(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/);
 var nameReg = new RegExp(/([^\s])/);
+
 function validation (){
 
   var name = document.forms["formOne"]["username"].value;
   var email = document.forms["formOne"]["input-email"].value;
   var password = document.forms["formOne"]["input-password"].value;
   var phoneNumber = document.forms["formOne"]["input-phoneNumber"].value;
+  
   if(nameReg.test(name) != true){
     alert("wrong name");
     return false;
@@ -17,6 +19,8 @@ function validation (){
     alert("wrong phone number");
     return false;
   }
+   
+  
   
   if(emailReg.test(email) != true){
     alert("wrong email");
@@ -30,6 +34,18 @@ function validation (){
   }
 
   alert("Registeration complete!");
+
+  sessionStorage.setItem("fullName",name);
+    console.log(sessionStorage.getItem("fullName"));
+
+  sessionStorage.setItem("phoneNumber",phoneNumber);
+  console.log(sessionStorage.getItem("phoneNumber"));
+
+  sessionStorage.setItem("userEmail",email);
+  console.log(sessionStorage.getItem("userEmail"));
+
+  sessionStorage.setItem("passwordSaved",password);
+  console.log(sessionStorage.getItem("passwordSaved"));
   return true;
 
 }
@@ -42,3 +58,27 @@ function changed(checkbox){
   }
 }
 
+function printOutput(){
+  var testingName = sessionStorage.getItem("fullName");
+  alert(`Hello ${testingName}, your email is ${sessionStorage.getItem("userEmail")}`)
+}
+
+function clearData(){
+  sessionStorage.clear();
+  // sessionStorage.removeItem("fullName");
+}
+
+function printAllItems() {
+
+  var key;
+  var data;
+  for(var i = 0; i < sessionStorage.length;i++){
+    key = "";
+    key += sessionStorage.key(i) + "\n";
+    data += sessionStorage.getItem(key) + "\n"; 
+  }
+  alert(key);
+  
+}
+
+// 
---
+++ JavaScript/21-Oct/index.js
@@ -0,0 +1,26 @@
+
+function validate(savingType) {
+  var key = document.getElementById("input-key").value;
+  var value =  document.getElementById("input-value").value;
+
+  if(value != null && key != null){
+    if (savingType == 'session'){
+      sessionStorage.setItem(key,value);
+      clearInput();
+    }else {
+      localStorage.setItem(key,value);
+      clearInput();
+    }
+  }
+}
+
+function clearInput(){
+  document.getElementById("input-key").value = "";
+  document.getElementById("input-value").value = "";
+
+}
+
+function clearData(){
+  sessionStorage.clear();
+  localStorage.clear();
+}
